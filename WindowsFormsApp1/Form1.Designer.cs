cc5498b2ccdfe9e7815aedf528ee125003cf75a6
Matvii Burchak <burchak@ut.ee>
2024-10-03 08:46:16 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.answer_lbl = new System.Windows.Forms.Label();
+            this.weight_txt = new System.Windows.Forms.TextBox();
+            this.height_txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 187);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(307, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Weight in kilograms";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Height in meters ";
+            // 
+            // answer_lbl
+            // 
+            this.answer_lbl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.answer_lbl.Location = new System.Drawing.Point(12, 104);
+            this.answer_lbl.Name = "answer_lbl";
+            this.answer_lbl.Size = new System.Drawing.Size(307, 64);
+            this.answer_lbl.TabIndex = 3;
+            // 
+            // weight_txt
+            // 
+            this.weight_txt.Location = new System.Drawing.Point(219, 13);
+            this.weight_txt.Name = "weight_txt";
+            this.weight_txt.Size = new System.Drawing.Size(100, 29);
+            this.weight_txt.TabIndex = 4;
+            // 
+            // height_txt
+            // 
+            this.height_txt.Location = new System.Drawing.Point(219, 53);
+            this.height_txt.Name = "height_txt";
+            this.height_txt.Size = new System.Drawing.Size(100, 29);
+            this.height_txt.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(335, 235);
+            this.Controls.Add(this.height_txt);
+            this.Controls.Add(this.weight_txt);
+            this.Controls.Add(this.answer_lbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "BMI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label answer_lbl;
+        private System.Windows.Forms.TextBox weight_txt;
+        private System.Windows.Forms.TextBox height_txt;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double weight = double.Parse(weight_txt.Text);
+                double height = double.Parse(height_txt.Text);
+                double body_index = Math.Round(weight / Math.Pow(height, 2), 2);
+
+                if (body_index < 18.5)
+                    answer_lbl.Text = $"{body_index}\n Status: Underweight";
+                else if (body_index > 25)
+                    answer_lbl.Text = $"{body_index}\n Status: Overweight";
+                else
+                    answer_lbl.Text = $"{body_index}\n Status: Optimal weight";
+            }
+            catch
+            {
+                answer_lbl.Text = "Invalid input. Please enter numeric values for weight and height.";
+            }
+        
+
+        }
+    }
+}
